5209ec3490d6caccf270e9107773fe5ae373ccf2
“张桂福” <904824669@qq.com>
2022-04-27 19:31:45 +0800

    s

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('managers')->insert(
+            [
+                [
+                    'name'     => '爱着雄雄的小华华',
+                    'phone'    => '12345678',
+                    'password' => Hash::make('12345678'),
+                    'department' => '华哥yyds',
+                    'level'   => 0,
+                    'api_token' => Str::random(60),
+                ]
+            ]);
+
     }
 }
