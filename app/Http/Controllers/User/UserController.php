aedbdcd948a3692a00b18223c625a636a4afe787
“张桂福” <904824669@qq.com>
2022-04-29 21:36:01 +0800

    s

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -90,6 +90,7 @@ class UserController extends Controller
             $type = 3;
         }
         $data = $this->_dataHandle($request);
+        print_r($request->input('status'));
         $data += ['status' => $request->input('status')];
         $model = Worker::query();
         if ($type == 1){
