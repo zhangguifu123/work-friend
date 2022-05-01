929d59687c75d162e1da1314ec827e6554b30ae4
“张桂福” <904824669@qq.com>
2022-05-01 14:30:38 +0800

    s

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
+use \Redis;
 
 class ImageController extends Controller
 {
