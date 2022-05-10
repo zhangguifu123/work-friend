150de29e547701c76d0d295777a6037648c12411
“张桂福” <904824669@qq.com>
2022-05-10 20:15:47 +0800

    s

--- app/Http/Controllers/WorkOrderContrller.php
+++ app/Http/Controllers/WorkOrderContrller.php
@@ -58,13 +58,13 @@ class WorkOrderController extends Controller
         if (is_array($data['salary'])) {
             if ($data['type'] == 'partTime'){
                 foreach ( $workOrderList as $workOrder ) {
-                    $workOrder['salary'] = json_decode($workOrder['salary'], true);
                     if ( $data['salary']['max'] >= $workOrder['salary'] && $data['salary']['min'] <= $workOrder['salary'] ) {
                         $newWorkOrderList[] = $workOrder;
                     }
                 }
             } else {
                 foreach ( $workOrderList as $workOrder ) {
+                    $workOrder['salary'] = json_decode($workOrder['salary'], true);
                     if ( $data['salary']['max'] >= $workOrder['salary']['max'] && $data['salary']['min'] <= $workOrder['salary']['min'] ) {
                         $newWorkOrderList[] = $workOrder;
                     }
