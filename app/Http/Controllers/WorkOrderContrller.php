c59ae5843f42d72342abb24c17b5f5406c8888bc
“张桂福” <904824669@qq.com>
2022-05-10 19:46:03 +0800

    s

--- app/Http/Controllers/WorkOrderContrller.php
+++ app/Http/Controllers/WorkOrderContrller.php
@@ -54,6 +54,7 @@ class WorkOrderController extends Controller
             ])
             ->toArray();
         $newWorkOrderList = [];
+        var_dump(is_array($data['salary']));
         if (is_array($data['salary'])) {
             if ($data['type'] == 'partTime'){
                 foreach ( $workOrderList as $workOrder ) {
