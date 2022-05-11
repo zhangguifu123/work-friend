5b2b174a637139cc74328e0d718aff28e452af3d
“张桂福” <904824669@qq.com>
2022-05-11 15:21:46 +0800

    s

--- app/Http/Controllers/WorkOrderContrller.php
+++ app/Http/Controllers/WorkOrderContrller.php
@@ -172,7 +172,7 @@ class WorkOrderController extends Controller
             "education"    => ["string"],
             "dateline"     => ["string"],
             "description"  => ["string", "nullable"],
-            "service_charge" => ["int"],
+            "service_charge" => ["integer"],
             "status"       => ["integer", 'nullable'],
         ];
         //是否缺失参数
